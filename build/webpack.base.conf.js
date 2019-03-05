42c37ce326deeeab7aacadb0946faf686dd215e4
omar <gongshichaoha@gmail.com>
2019-03-05 19:45:50 +0800

    Fix the background network interface request

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -70,7 +70,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.API_BASE': '"http://127.0.0.1:3000"'
+      'process.env.API_BASE': '"http://47.95.215.162"'
     })
   ],
   node: {
